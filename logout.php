4c07211014bae98482a61b3f1b5c9bbfd619bb84
Paul Sijpkes <paul.sijpkes@gmail.com>
2014-10-24 15:30:29 +1100

    stripped out unused files

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('COOKIE_SESSION', true);
-require_once("config.php");
-require_once("lib/lms_lib.php");
-session_start();
-session_unset();
-deleteSecureCookie();
-
-header('Location: index.php');
--- samples/settings/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$REGISTER_LTI2 = array(
-"name" => "Settings Unit Test Tool",
-"short_name" => "Settings Test",
-"description" => "This tool allows you to exercise the LTI 2.x JSON Settings API."
-);
-
